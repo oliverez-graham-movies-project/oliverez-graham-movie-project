d2660822120402dcf09d7f215fea5bae7b863198
Theodoric Graham <theodoricgraham@gmail.com>
2023-05-08 16:16:16 -0500

    starting on load animation

--- movies.js
+++ movies.js
@@ -1,21 +1,41 @@
-(function() {
+(function () {
     'use strict'
 
+    //Test fetch
     fetch('https://mysterious-flat-dawn.glitch.me/movies').then(res => res.json()).then(res => console.log(res))
 
 
+    //display loading
+
+
     //post request TODO needs cleaning
-    const moviePost = {title: 'Godzilla', body: 'Monster movie'};
-    const url = 'https://mysterious-flat-dawn.glitch.me/movies';
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(moviePost),
-    };
-    fetch(url, options)
-        .then(res => res.json()).then(res => console.log(res))
-        .catch(/* handle errors */);
+    // const moviePost = {title: 'Godzilla', body: 'Monster movie'};
+    // const url = 'https://mysterious-flat-dawn.glitch.me/movies';
+    // const options = {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(moviePost),
+    // };
+    // fetch(url, options)
+    //     .then(res => res.json()).then(res => console.log(res))
+    //     .catch(/* handle errors */);
+
+    //Delete request
+
+    // const moviePost = {title: 'Godzilla', body: 'Monster movie'};
+    // const url = 'https://mysterious-flat-dawn.glitch.me/movies';
+    // const options = {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(moviePost),
+    // };
+    // fetch(url, options)
+    //     .then(res => res.json()).then(res => console.log(res))
+    //     .catch(/* handle errors */);
+
 
 })()
