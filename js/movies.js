4048f7d9d5ac895824ffb33243c6b8e34a396422
Theodoric Graham <theodoricgraham@gmail.com>
2023-05-11 02:51:15 -0500

    fixed card height issue, and added search functionality

--- js/movies.js
+++ js/movies.js
@@ -5,10 +5,16 @@
 
     // ************OMDB FETCH*********** //
     const fetchDBTitle = async title => {
-        const urlString = `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&t=${title}`
-        const response = await fetch(urlString)
-        const movieInfo = await response.json()
-        return movieInfo
+        try {
+            const urlString = `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&t=${title}`
+            const response = await fetch(urlString)
+            const movieInfo = await response.json()
+            return movieInfo
+        } catch (err) {
+            console.log('oh no')
+            return alert(err)
+        }
+
     }
 
     // ***********LOADER WHEEL********** //
@@ -24,58 +30,92 @@
     }
 
     const fetchHandler = async () => {
-        displayLoading()
-        // UNCOMMENT FOR TIMER
-        // setTimeout(() => {
-        //             hideLoading()
-        //             //     half a second delay
-        //         }, 4000)
-        const moviesResponse = await fetch(movieUrl)
-        const moviesJSON = await moviesResponse.json()
-        await createMovieCards(moviesJSON)
-        hideLoading()
+        try {
+            displayLoading()
+            // UNCOMMENT FOR TIMER
+            // setTimeout(() => {
+            //             hideLoading()
+            //             //     half a second delay
+            //         }, 4000)
+            const moviesResponse = await fetch(movieUrl)
+            const moviesJSON = await moviesResponse.json()
+            return moviesJSON
+        } catch(err) {
+            console.log('oh no')
+            return alert(err)
+        }
+
+        // await createMovieCards(moviesJSON)
+        // hideLoading()
+
+
+    }
+
+    const fetchAndCreateCards = async (arr) => {
+        try {
+            await createMovieCards(arr)
+            hideLoading()
+        } catch(err) {
+            console.log('oh no')
+            return alert(err)
+        }
+
     }
 
     // ***********CREATE CARDS********** //
     // TODO CLEAN UP FUNCTION, ASYNC MAY NOT BE NECESSARY
+
+
     const createMovieCards = async arr => {
-        let markup = ''
-        const newArr = arr.map(async movie => {
-            const movieTitle = movie.title
-            const movieDB = await fetchDBTitle(movieTitle)
-            return markup += `
-            <div class="card mt-3" id="grad2">
-                <div class="top-holder d-flex flex-column align-items-center ">
-                </div>
+        try {
+            const array = await arr
+            let markup = ''
+            // console.log(array)
+            // const testArr = [...array]
+            // console.log(testArr)
+            const newArr = array.map(async movie => {
+                const movieTitle = movie.title
+                const movieDB = await fetchDBTitle(movieTitle)
+                markup += `
+            <div class="card card-set-height mt-3" id="grad2">
                 <p class="genre text-center"><span class="badge rounded-pill bg-white">${movieDB.Genre}</span></p>
                 <img src="${movieDB.Poster}" class="card-img-top" alt="${movieDB.Title}">
-                <div class="card-body">
-                <p class="rating d-flex justify-content-center"><i class="fa-sharp fa-solid fa-star"></i> Rating: ${movie.rating}/10</p>
-                <hr>
-                    <p class="director">Director: ${movieDB.Director}</p>
+                <div class="card-body d-flex flex-column justify-content-between">
+                    <p class="rating d-flex justify-content-center"><i class="fa-sharp fa-solid fa-star"></i> Rating: ${movie.rating}/10</p>
+                    <hr>
+                    <p class="card-info">Title: ${movieDB.Title}</p>
+                    <hr>
+                    <p class="director card-info">Director: ${movieDB.Director}</p>
                     <hr>
                     <p>
-  <a class="dropdown-toggle" data-bs-toggle="collapse" href="#collapse-${movie.id}" role="button" aria-expanded="false" aria-controls="collapseExample">
-    Plot
-  </a>
-</p>
-<div class="collapse" id="collapse-${movie.id}">
-  <div class="card card-body">
-   <p class="synopsis">${movieDB.Plot}</p>
-  </div>
-</div>
-                    
-                    
-                      <button type="button" class="btn btn-primary deleteBtn mt-2" id="delete-${movie.id}">Delete</button>
+                        <a class="dropdown-toggle" data-bs-toggle="collapse" href="#collapse-${movie.id}" role="button" aria-expanded="false" aria-controls="collapseExample">
+                        Plot
+                        </a>
+                    </p>
+                    <div class="collapse" id="collapse-${movie.id}">
+                        <div class="card card-body">
+                            <p class="synopsis">${movieDB.Plot}</p>
+                        </div>
+                    </div>
+                    <div class="">
+                    <button type="button" class="btn btn-primary deleteBtn mt-2" id="delete-${movie.id}">Delete</button>
                      <button class="btn btn-primary editBtn mt-2" id="patch-${movie.id}">Edit</button>
+                    </div>
+                     
                 </div>
             </div>
             `
-        })
+            })
+            await Promise.all(newArr)
+            $('.container').html(markup)
+
+        } catch (err) {
+            console.log('oh no')
+            console.log(err)
+        }
 
         //  <button class="btn btn-primary editBtn mt-2" id="patch-${movie.id}">Edit</button>
-        await Promise.all(newArr)
-        $('.container').html(markup)
+
     }
 
     // ************POST REQUEST FUNCTIONS*********** //
@@ -89,7 +129,7 @@
         };
         fetch(movieUrl, postOptions)
             .then(res => res.json()).then(json => {
-            fetchHandler()
+            void fetchAndCreateCards(fetchHandler())
         })
             .catch(/* handle errors */);
     }
@@ -127,7 +167,7 @@
                 // PULLING JUST THE NUMBER FROM THE STRING, TO USE AS ID
                 const id = e.target.id.replace(/[^0-9]/g, "");
                 fetch(`https://mysterious-flat-dawn.glitch.me/movies/${id}`, deleteOptions).then(res => {
-                    fetchHandler()
+                    void fetchAndCreateCards(fetchHandler())
                 })
                     .catch(/* handle errors */);
             }
@@ -148,8 +188,8 @@
         };
         fetch(`https://mysterious-flat-dawn.glitch.me/movies/${id}`, patchOptions)
             .then(res => res.json()).then(json => {
-            console.log(json)
-            fetchHandler()
+            // console.log(json)
+            void fetchAndCreateCards(fetchHandler())
         })
             .catch(/* handle errors */);
     }
@@ -209,13 +249,28 @@
 
     // ************NAV SEARCH*********** //
     //need a function that takes an array of our movie data, then sorts it by name each change
+    const filterMovies = async input => {
+        // console.log(input)
+        const movieArray = await fetchHandler()
+        hideLoading()
+        const filteredMovies = movieArray.filter(movie => {
+            return movie.title.includes(input)
+        })
+        // console.log(filteredMovies)
+        fetchAndCreateCards(filteredMovies)
+
+    }
 
     //Select form input
-    console.log($('.search-form'));
+    // console.log($('.search-form'));
+    $('.search-form').on('keyup', () => {
+        let input = $('.search-form').val()
+        filterMovies(input)
+    })
 
 
     // INITIAL FETCH CALL
-    fetchHandler()
+    void fetchAndCreateCards(fetchHandler())
 
 
 })()
